34dd8830454df43db5b02297c8416a5e85bb43e7
Nate <daviesnathanael45@gmail.com>
2025-12-04 15:25:24 +0000

    started making chunks, for procduraly generating areas in the game

---
+++ include/chunks.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+
+namespace chunks {
+    inline const std::vector<std::vector<int>>  lvl = {
+            {0,0,0,0,1},
+            {0,0,0,0,1},
+            {0,0,0,0,1},
+            {0,0,0,0,1},
+            {0,0,0,0,1},
+            {0,0,0,0,1},
+            {0,0,0,0,1},
+            {0,0,0,0,1},
+            {2,2,2,2,2}
+        };
+    }
--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,14 @@ int main(){
 
     const Color SKY = {139, 185, 201, 255};
 
-    Player player(600.0f,200.0f);
+    Player player(100.0f,1000.0f);
 
     Ground ground;
 
-
     //adding the camera
     Camera2D camera =  {0};
-    camera.target = {player.position.x + player.size /2, player.position.y + player.size/2};
-    camera.offset = { screenW/2.0f, screenH/1.5f};
+    camera.target = {player.position.x + (player.size*16)/2, player.position.y + (player.size*16)/2};
+    camera.offset = { screenW/2.0f, screenH/2.0f};
     camera.zoom = 1.2f;
 
     //while running
@@ -40,6 +39,9 @@ int main(){
         if (IsKeyDown(KEY_LEFT)) player.acceleration.x += -35.0f;
         if (IsKeyDown(KEY_UP)) if (!player.inair) player.acceleration.y = -1500.0f;
 
+        if (IsKeyDown(KEY_EQUAL)) camera.zoom += 0.1f;
+        if (IsKeyDown(KEY_MINUS)) camera.zoom -= 0.1f;
+
         //move is for x direction fall is for y direction
         player.Move(deltaTime);
         player.Fall(deltaTime);
@@ -47,8 +49,8 @@ int main(){
         //the player collsion
         player.Collide(ground);
 
-        //sets the camera traget
-        camera.target = {player.position.x + player.size /2, player.position.y + player.size/2};
+        //sets the camera target to the new player position
+        camera.target = {player.position.x + (player.size*16) /2, player.position.y + (player.size*16)/2};
 
         //draw what is on the screen
         BeginDrawing();
@@ -59,6 +61,12 @@ int main(){
         //draw all the ground
         ground.Draw();
 
+        //draw the player
+        player.Draw();
+
+        EndMode2D();//end of drawing in the world
+
+        // draw the debug stuff AFTER the camera is off so it draws to a set place on the screen
         //This  stuff is fro DEBUGGING PRESS [0] to enable debugging menu unside the game
         if (debug_mode){
             char debug_onground[30];
@@ -68,15 +76,16 @@ int main(){
             Result colRight = ground.Collide((player.position.x+14*player.size),player.position.y+16*player.size-1);
             Result colLeft = ground.Collide((player.position.x),player.position.y+16*player.size-1);
             //debug to see what tile the player is colliding with
+            //these need to be inside the camera placement
+            BeginMode2D(camera);
             DrawRectangle(col.x* 32 *ground.groundScale,col.y* 32 *ground.groundScale,32*ground.groundScale,32*ground.groundScale,RED);
             DrawRectangle(colRight.x* 32 *ground.groundScale,colRight.y* 32 *ground.groundScale,32*ground.groundScale,32*ground.groundScale,GREEN);
             DrawRectangle(colLeft.x* 32 *ground.groundScale,colLeft.y* 32 *ground.groundScale,32*ground.groundScale,32*ground.groundScale,BLUE);
+            player.Draw();
+            EndMode2D();
         }
 
-        //draw the player
-        player.Draw();
-
-        EndMode2D();
+        //END OF THE DRAWING
         EndDrawing();
     }
 
